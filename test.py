f818127c5ab9e3992caeb0b13e5895c0ecf7ce95
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2026-07-03 18:12:41 +0530

    ml flow uri produced

---
+++ test.py
@@ -0,0 +1,7 @@
+import mlflow
+print(mlflow.get_tracking_uri())
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print("Printing new tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("\n")
